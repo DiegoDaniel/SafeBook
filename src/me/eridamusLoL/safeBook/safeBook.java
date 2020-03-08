3039b6239693a2c784420cc848f2e173e06c5a1d
DiegoDaniel <galarza.diego@hotmail.com>
2020-03-07 21:52:19 -0300

    fixed everything

--- src/me/eridamusLoL/safeBook/safeBook.java
+++ src/me/eridamusLoL/safeBook/safeBook.java
@@ -10,8 +10,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTakeLecternBookEvent;
 
-
-
 public class safeBook implements Listener{
 
 	private Main main;
@@ -22,29 +20,33 @@ public class safeBook implements Listener{
 		Player player = e.getPlayer();
 		Block block = e.getClickedBlock();
 		Action a = e.getAction();
-		if(a.equals(Action.LEFT_CLICK_BLOCK) && block.getType().equals(Material.LECTERN) && player.getInventory().getItemInMainHand().getType().equals(Material.AIR) && player.isOp()) {
-			//Check if XYZ safeBook exist. 
-			int x = block.getX();
-			int y = block.getY();
-			int z = block.getZ();
-			String blockXYZ = Integer.toString(x).concat(Integer.toString(y)).concat(Integer.toString(z));
-			System.out.println("STRING XYZ: "+ blockXYZ);
-			
-			if(main.getConfig().toString().length() > 0 && main.getConfig().getConfigurationSection("safeBooksXYZ") != null) {
-				ConfigurationSection sec = main.getConfig().getConfigurationSection("safeBooksXYZ");
-		        if(null != sec && sec.contains(blockXYZ)) {
-		        	sec.set(blockXYZ, null);
-		        	main.saveConfig();
-		        	player.sendMessage("This book is not protected anymore.");
-		        }else{
-		        	sec.set(blockXYZ + ".x", x);
-		        	sec.set(blockXYZ + ".y", y);
-		        	sec.set(blockXYZ + ".z", z);
-		        	main.saveConfig();
-		        	player.sendMessage("This book is now protected.");
-		        }
-			}else {
-				player.sendMessage("safeBook's config file is broken. Please re-install plugin to fix it.");
+		if(a.equals(Action.LEFT_CLICK_BLOCK) && block.getType().equals(Material.LECTERN) 
+				&& player.getInventory().getItemInMainHand().getType().equals(Material.AIR) 
+				&& player.isOp()) {
+			String blockData = block.getBlockData().getAsString();
+			if(blockData.contains("has_book=true")) { //Plugin works only if Lectern has a book on it.
+				//Check if XYZ safeBook exist. 
+				int x = block.getX();
+				int y = block.getY();
+				int z = block.getZ();
+				String blockXYZ = Integer.toString(x).concat(Integer.toString(y)).concat(Integer.toString(z));
+				System.out.println("STRING XYZ: "+ blockXYZ);
+				if(main.getConfig().toString().length() > 0 && main.getConfig().getConfigurationSection("safeBooksXYZ") != null) {
+					ConfigurationSection sec = main.getConfig().getConfigurationSection("safeBooksXYZ");
+			        if(null != sec && sec.contains(blockXYZ)) {
+			        	sec.set(blockXYZ, null);
+			        	main.saveConfig();
+			        	player.sendMessage("This book is not protected anymore.");
+			        }else{
+			        	sec.set(blockXYZ + ".x", x);
+			        	sec.set(blockXYZ + ".y", y);
+			        	sec.set(blockXYZ + ".z", z);
+			        	main.saveConfig();
+			        	player.sendMessage("This book is now protected.");
+			        }
+				}else {
+					player.sendMessage("safeBook's config file is broken. Please re-install plugin to fix it.");
+				}
 			}
 		}
 	}
@@ -54,10 +56,12 @@ public class safeBook implements Listener{
 		Player player = e.getPlayer();
 		Block block = e.getLectern().getBlock();
 		String blockXYZ = Integer.toString(block.getX()).concat(Integer.toString(block.getY())).concat(Integer.toString(block.getZ()));
-		ConfigurationSection sec = main.getConfig().getConfigurationSection("safeBooksXYZ");
-        if(sec.contains(blockXYZ)) {
-        	player.sendMessage("You can't take this book!");
-    		e.setCancelled(true);
+		if(main.getConfig().toString().length() > 0 && main.getConfig().getConfigurationSection("safeBooksXYZ") != null) {
+			ConfigurationSection sec = main.getConfig().getConfigurationSection("safeBooksXYZ");
+        	if(sec.contains(blockXYZ)) {
+            	player.sendMessage("You can't take this book!");
+        		e.setCancelled(true);
+            }
         }
 	}
 	
